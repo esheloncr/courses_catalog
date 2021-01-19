ab8fe8aa4005f4ae2aefb0a9ad3936f3c387cef9
esheloncr <igoninalexeynn@gmail.com>
2021-01-19 18:11:10 +0300

    added UrlField, and admin redact

--- catalog/courses/admin.py
+++ catalog/courses/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 from .models import Course
 # Register your models here.
 
-admin.site.register(Course)
+
+class AdminCourse(admin.ModelAdmin):
+    list_display = ('title', 'start_date', 'end_date','course_url')
+    list_filter = ('course_id', 'start_date', 'end_date')
+    fieldsets = (
+        (None, {
+            'fields': ('title','description','course_url')
+        }),
+        ('Course date', {
+            'fields': ('start_date', 'end_date')
+        }),
+    )
+
+
+admin.site.register(Course,AdminCourse)
--- catalog/courses/models.py
+++ catalog/courses/models.py
@@ -4,10 +4,15 @@ from django.db import models
 
 
 class Course(models.Model):
+    course_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=128)
     description = models.TextField(max_length=512)
+    course_url = models.URLField(max_length=200, default="#")
     start_date = models.DateField()
     end_date = models.DateField()
 
     def __str__(self):
-        return self.title, self.description, self.start_date
+        return self.title
+
+    def __repr__(self):
+        return self.course_id, self.title, self.description,self.course_url, self.start_date, self.end_date
